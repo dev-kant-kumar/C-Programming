e725538d4db9772727d55f1c1e7a798d08188c75
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-10-16 13:51:51 +0530

    Adding first program of string

---
+++ string1.c
@@ -0,0 +1,11 @@
+// This is first program of string 
+#include<stdio.h>
+int main()
+ {
+    char name[]="Dev Kant Kumar";
+    for(int i=0;name[i]!='\0'; i++)
+     {
+        printf("%c",name[i]);
+     }
+    return 0;
+ }
