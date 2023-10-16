de16db0ad9bc59a815028eb3f38d4c3a18d958f8
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-10-16 12:55:35 +0530

    improve code functionality

--- array1.c
+++ array1.c
@@ -5,13 +5,13 @@ int main()
     float price[3],final_price[3];
     for(int i=0;i<3;i++)
      {
-       printf("Enter the price of iteam %d : ",i);
+       printf("Enter the price of iteam %d : ",i+1);
        scanf("%f",&price[i]);
        final_price[i]=price[i]+price[i]*18/100 ;
     }
     for(int i=0;i<3;i++)
      {
-        printf("Final price of iteam %d is %f \n",i,final_price[i]);
+        printf("Final price of iteam %d is %f \n",i+1,final_price[i]);
      }
   return 0;   
  }
