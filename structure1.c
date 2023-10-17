eb7ec1c3e39383efc1e5555b982ee6e2decc89cb
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-10-17 18:20:24 +0530

    Adding first program on structure

---
+++ structure1.c
@@ -0,0 +1,40 @@
+// Very first program on structure 
+#include<stdio.h>
+struct student {
+    int rollno;
+    char name[50];
+    float height;
+};
+void studentDataEntry( struct student *a);
+void checkRecords(struct student a );
+
+
+int main()
+ {
+    int noOfRecords;
+    printf("How many records you want to enter :");
+    scanf("%d",&noOfRecords);
+    struct student records[noOfRecords];
+    for(int i=0;i<noOfRecords;i++)
+     {
+        studentDataEntry(&records[i]);
+     }
+    printf("Students Records \n");
+    for(int i=0;i<noOfRecords;i++)
+     {
+        checkRecords(records[i]);
+     } 
+ }
+ void studentDataEntry( struct student *a)
+  {
+    printf("Enter your roll no :");
+    scanf("%d",&a->rollno);
+    printf("Enter your name :");
+    scanf(" %[^\n]s",a->name);
+    printf("Enter your height :");
+    scanf("%f",&a->height);
+  }
+  void checkRecords(struct student a)
+   {
+    printf("Roll : %d , Student Name : %s ,Height : %f \n",a.rollno,a.name,a.height);
+   }
