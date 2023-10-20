53652039e7bc100a03bd30c07f52fc426e513680
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-10-20 21:06:08 +0530

    fix the problem in code and extending its functionality

--- array6.c
+++ array6.c
@@ -1,45 +1,93 @@
-// Write a program for Matrix multiplication
+// Write a program for matrix multiplication
 #include<stdio.h>
+void mulCheck(int a, int b, int c , int d);
+void MatrixMul( int a, int b, int c, int d);
+
 int main()
  {
-    int a[3][3],b[3][3],c[3][3];
-    printf("Enter elements of first matrix \n");
-    for(int i=0;i<3;i++)
-     {
-        for(int j=0;j<3;j++)
-         {
-            printf("Enter the %d element of first matrix :",i+1):
-            scanf("%d",a[i][j]);
-         }
-       
-     }
-    for(int i=0;i<3;i++)
-     {
-        for(int j=0;j<3;j++)
-         {
-            printf("Enter the %d element of second matrix :",i+1);
-            scanf("%d",b[i][j]);
-         }
-     } 
-    for(int i=0;i<3;i++)
-     {
-      for(int j=0;j<3;j++)
-       c[i][j]=0;
-       for(int k=0;k>3;k++)
-       {
-         c[i][j]+=a[i][k]* b[k][j];
-       }
-     }
-    printf("Multiplication of Matrix A and B is below \n");
-    for(int i=0;i<3;i++)
-     {
-      for(int j=0;j<3;j++)
-       {
-         printf("%d \t",c[i][j]);
-       }
-      printf("\n");
-     }
-   return 0;
+   // arow= no of row of first matrix, bcolumn= no of columns of first matrix and same for b as second matrix
+     int arow,acolumn,brow,bcolumn;
+     printf("Enter the no of rows for first matrix : ");
+     scanf("%d",&arow);
+     printf("Enter the no of columns for first matrix : ");
+     scanf("%d",&acolumn);
+     printf("\n");
+     printf("Enter the no of rows of second matrix : ");
+     scanf("%d",&brow);
+     printf("Enter the no of columns for second matrix : ");
+     scanf("%d",&bcolumn);
+     mulCheck(arow,acolumn,brow,bcolumn);
+     return 0;
+ } 
 
- }
+// This function check whether matrix multiplication is possible or not 
  
+ void mulCheck( int a, int b, int c, int d)
+  {
+    if(b==c)
+     {
+       MatrixMul(a,b,c,d);
+     }
+    else
+     {
+      printf("\n");
+      printf("Matrix Multiplication is not possible with entered no of rows and columns data \n");
+      printf("Columns of first matrix must be same as rows of second matrix \n\n");
+     } 
+  }
+
+  // This function takes input of matrix elements, perform matrix multiplication and print it.
+
+void MatrixMul( int a, int b, int c, int d)
+  {
+    int M1[a][b],M2[c][d],P[a][d]; 
+    // M1= first matrix, M2= second matrix, P is resultant matrix
+    printf("\n");
+    printf("Enter %d elements of first matrix  \n\n",a*b);
+    for(int i=0;i<a;i++)
+     {
+       for(int j=0;j<b;j++)
+        {
+         printf("Enter %d element of %d row : ",j+1,i+1);
+         scanf("%d",&M1[i][j]);
+        }
+     }
+    printf("\n"); 
+    printf("Enter %d elements of second matrix \n\n",c*d);
+    for(int i=0;i<c;i++)
+     {
+       for(int j=0;j<d;j++)
+        {
+         printf("Enter %d element of %d row : ",j+1,i+1);
+         scanf("%d",&M2[i][j]);
+        }
+     }
+    //  Matrix Multiplication part 
+     for(int i=0;i<a;i++)
+      {
+        for(int j=0;j<d;j++)
+         {
+           P[i][j]=0;
+           for(int k=0;k<d;k++)
+            {
+             P[i][j]+=M1[i][k]*M2[k][j];
+            }
+         }
+      }
+    // Printing Matrix Multiplication 
+    printf("\n");
+    printf("Multiplication of Matrices are below \n");
+     for(int i=0;i<b;i++)
+      {
+        for(int j=0;j<c;j++)
+         {
+          printf("%d \t",P[i][j]);
+         }
+         printf("\n");
+      } 
+  }      
+
+     
+
+   
+   
