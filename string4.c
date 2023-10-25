50235a1cdac7bf5a1176a595b75aa7081169a57a
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-10-25 11:45:49 +0530

    adding a new program on string

---
+++ string4.c
@@ -0,0 +1,10 @@
+// String using pointer
+// If you are declearing string using pointer you can change its value later.
+#include<stdio.h>
+int main()
+ {
+    char *name ="Dev Kant Kumar";
+    name="Dev Kumar";
+    printf("Your name is %s \n",name);
+    return 0;
+ }
