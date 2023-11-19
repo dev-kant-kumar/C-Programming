213a1890a8f31292e0a782c2ae8cf3f500540a45
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-11-19 14:00:12 +0530

    Adding string program file

--- string6.c
+++ string6.c
@@ -1,1 +1,22 @@
-// Find the salted form of a password entered by user 
+// Find the salted form of a password entered by user if the salt is "123" & added at the end.
+#include<stdio.h>
+#include<string.h>
+
+void salting(char password[]);
+int main()
+ {
+    char input_password[17];
+    printf("Enter your password: ");
+    scanf("%s",input_password);
+    salting(input_password);
+    return 0; 
+ }
+
+void salting(char password[]) 
+ {
+    char salt[]="123";
+    char salted_password[20];
+    strcpy(salted_password,password);
+    strcat(salted_password,salt);
+    printf("Salted password : %s \n",salted_password);
+ }
